51de2aba940f6c230245bead1646a3fd03392c23
devesh@760 <drajawat760@gmail.com>
2025-02-24 01:02:52 +0530

    commander

--- bin/index.js
+++ bin/index.js
@@ -1,26 +1,30 @@
 #!/usr/bin/env node
 
+const { Command } = require("commander");
 const stashBackup = require("../src/stashBackup");
 const stashRestore = require("../src/stashRestore");
 
-async function main() {
-  const args = process.argv.slice(2);
+const program = new Command();
 
-  if (args[0] === "init") {
+program
+  .name("git-stash-sync")
+  .description("A CLI tool to backup and restore Git stashes")
+  .version("1.0.0", "-v, --version", "Output the current version");
+
+program
+  .command("init")
+  .description("Restore stashes from the remote tar backup")
+  .action(async () => {
     console.log("Initializing stash sync...");
     await stashRestore();
-  } else if (args[0] === "backup") {
-    console.log("Backing up stashes as a compressed tar archive...");
-    await stashBackup();
-  } else {
-    console.log("Usage:");
-    console.log(
-      "  git-stash-sync init   # Restore stashes from the remote tar backup"
-    );
-    console.log(
-      "  git-stash-sync backup # Manually trigger stash backup as a tar archive"
-    );
-  }
-}
+  });
 
-main();
+program
+  .command("backup")
+  .description("Manually trigger stash backup as a tar archive")
+  .action(async () => {
+    console.log("Backing up stashes...");
+    await stashBackup();
+  });
+
+program.parse(process.argv);
